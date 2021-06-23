dbbdc53e99353384f593caf9fffe405e205d0282
roberto-cardoso <robertocardoso1717@gmail.com>
2021-06-22 21:19:21 -0300

    Update aumento-de-salario.py
    o aumento era de 15% e não de 5%

--- desafio-013/aumento-de-salario.py
+++ desafio-013/aumento-de-salario.py
@@ -3,7 +3,7 @@ print('"Aumento"')
 print(':'*50)
 
 salario = float(input('Quanto recebe o funcionário? '))
-novo_salario = salario+(salario*(5/100))
+novo_salario = salario+(salario*(15/100))
 print('O novo salário dele é de R${}.'.format(novo_salario))
 
 print(':'*50)
