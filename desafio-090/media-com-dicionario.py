8e18547864cc08c1065aa2bce8f097e8ff031137
Roberto Cardoso <robertocardoso1717@gmail.com>
2022-05-24 00:33:39 -0300

    mais desafios de Python

---
+++ desafio-090/media-com-dicionario.py
@@ -0,0 +1,12 @@
+aluno = {}
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['media'] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao']
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
