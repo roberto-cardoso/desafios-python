0961d96c1e7ebc25156ed40bbfc092381c779ddc
Roberto Cardoso <robertocardoso1717@gmail.com>
2022-07-24 00:31:39 -0300

    falei que tava acabando. falta pouquinho agora ;)

---
+++ desafio-100/sorteio-e-soma.py
@@ -0,0 +1,25 @@
+from random import randint
+from time import sleep
+
+
+def sorteia(lista):
+    print('Sorteando os números da lista: ', end='')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
+
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}.')
+
+
+numeros = list()
+sorteia(numeros)
+somaPar(numeros)
